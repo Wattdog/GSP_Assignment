7f27eb97fe088a71acf0d6c944ef647e5df03979
Aaron <aaronharrisonwatt1@gmail.com>
2021-03-09 14:09:00 +0000

    Added Scriptable Objects for guns

---
+++ Shooting Test Environment/Assets/Scripts/GunData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun")]
+public class GunData : ScriptableObject
+{
+    public float damage;
+    public float range;
+}
