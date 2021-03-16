36ca5ef7d62957e3caf20de10299c828cc15ccc1
Chloe <78434392+EmoChicken94@users.noreply.github.com>
2021-03-16 10:55:01 +0000

    Mansion Blockout

---
+++ MansionBlock out/Assets/Scripts/Camara_Movment.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara_Movment : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public Transform shotPos;
+    public float shotForce = 1000f;
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensativity = 2.0f;
+    public float smoothing = 2.0f;
+    GameObject Character;
+	// Use this for initialization
+	void Start () {
+        Character = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensativity * smoothing, sensativity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        Character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Character.transform.up);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+            shot.AddForce(shotPos.forward * shotForce);
+        }
+	}
+}
---
+++ MansionBlock out/Assets/Scripts/Player_movment_01.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movment_01 : MonoBehaviour {
+
+    public float speed = 10.0f;
+    private float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+    private Rigidbody rb;  
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation  = true;
+        rb.useGravity = false;
+    } 
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown ("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            Vector3 velocity = rb.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            if (canJump && Input.GetButton("Jump")) 
+            {
+                rb.velocity = new Vector3(velocity.x, CaculateJUmpVerticalSpeed(), velocity.z);
+            }
+        }
+        rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CaculateJUmpVerticalSpeed ()
+    {
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+
+
+}
