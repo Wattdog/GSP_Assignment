6a6442580865bf8a9f4c7e84ffe01dc60c25a229
Chloe <s5207215@bournemouth.ac.uk>
2021-03-23 14:15:43 +0000

    UI

---
+++ GSP UI  Menus/Assets/Scripts/H_and_S_cntrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_and_S_cntrl : MonoBehaviour
+{
+
+    public ProgressBar healthBar;
+   
+
+    public ProgressBar StaminaBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.BarValue = 100;
+        Debug.Log ("health = 100");
+        StaminaBar.BarValue = 100;
+        Debug.Log("stamina = 100");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ GSP UI  Menus/Assets/Scripts/Menu_Cntrl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Cntrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LdLvlOne()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LdLvlTwo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void LdLvlThree()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void LdLvlFour()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+}
+
